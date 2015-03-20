6b7b038f515d9f7c761e396addca174bb6193f3d
mikehajj <mike.hajj@gmail.com>
2015-03-20 14:36:20 +0200

    first commit for soajs website contact us service.

---
+++ README.md
@@ -0,0 +1,15 @@
+# soajs.website.ui.contact-us
+[![Build Status](https://travis-ci.org/soajs/soajs.website.contactus.svg?branch=master)](https://travis-ci.org/soajs/soajs.website.contactus)
+[![Coverage Status](https://coveralls.io/repos/soajs/soajs.website.contactus/badge.png)](https://coveralls.io/r/soajs/soajs.website.contactus)
+
+SOAJS contact us service is a service that sends email based on the specified recipient address in the configuration file.
+This services uses the mailer of SOAJS.
+
+##Installation
+Install soajs.website.ui before you can run the examples.
+
+```sh
+$ npm install soajs.website.contactus
+$ cd soajs.website.contactus
+$ node .
+```
---
+++ config.js
@@ -0,0 +1,60 @@
+'use strict';
+
+module.exports = {
+	serviceName: "contactUs",
+	"errors": {
+		"100": "Failed to send email"
+	},
+	"mail": {
+		"to": "mike@soajs.org",
+		"subject": "Message from SOAJS Website Contact Us Section",
+		headers: {
+			type: 'object',
+			required: false,
+			additionalProperties: {
+				'type': 'string'
+			}
+		},
+		"transport": {
+			"type": "sendmail", // smtp , sendmail, direct
+			"options": {
+				"path": "/usr/sbin/sendmail"
+			}
+		}
+		
+	},
+	"schema": {
+		"/sendMessage": {
+			"name": {
+				"source": ['body.name'],
+				"required": true,
+				"validation": {
+					"type": "string"
+				}
+			},
+			"purpose": {
+				"source": ['body.purpose'],
+				"required": true,
+				"validation": {
+					"type": "string"
+				}
+			},
+			"email": {
+				"source": ['body.email'],
+				"required": true,
+				"validation": {
+					"type": "string",
+					format: "email"
+				}
+			},
+			"message": {
+				"source": ['body.message'],
+				"required": true,
+				"validation": {
+					"type": "string"
+				}
+			}
+		}
+
+	}
+};
